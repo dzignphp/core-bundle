18832217422dea5ed7956c471293c2619bb583bd
Jérôme <jdrzewinski@kernix.com>
2020-02-13 10:30:50 +0100

    commit

---
+++ Command/CreateAdminCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dzign\AdminBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateAdminCommand extends Command
+{
+    protected static $defaultName = 'dzign:admin:create';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Create an admin.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        return 1;
+    }
+}
---
+++ DzignAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dzign\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DzignAdminBundle extends Bundle
+{
+}
